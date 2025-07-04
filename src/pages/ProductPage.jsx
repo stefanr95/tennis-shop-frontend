1f605e273afc55b31f1327d1ce36d4b5fa3ca685
Stefan Radomirović <radomirovicstefan5@gmail.com>
2025-07-04 14:44:04 +0200

    ProductPage small change and add ProductPage.css

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,22 +1,23 @@
 import { useEffect, useState } from "react";
 import axios from "../api/axios";
 import ProductCard from "../components/ProductCard";
-import "../styles/RegisterPage.css";
+import "../styles/ProductPage.css";
 
 const ProductPage = () => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-   axios.get("/products")
-  .then(res => setProducts(res.data.content)) // ako se koristi Pageable
-  .catch(err => console.error(err));
+    axios
+      .get("/products")
+      .then((res) => setProducts(res.data.content))
+      .catch((err) => console.error(err));
   }, []);
 
   return (
     <div className="product-page">
-      <h2>Svi proizvodi</h2>
-      <div className="product-list">
-        {products.map(product => (
+      <h2 className="product-page-title">Svi proizvodi</h2>
+      <div className="product-grid">
+        {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
